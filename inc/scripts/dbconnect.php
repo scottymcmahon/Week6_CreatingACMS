8343f931a1ec93be383f86e807010ebdc5c07913
scottymcmahon <scottmcmahondev@outlook.com>
2016-10-31 15:26:12 +0000

    sec Commit

---
+++ inc/scripts/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1404436
+ * Date: 31/10/2016
+ * Time: 14:40
+ */
---
+++ inc/scripts/footer.php
@@ -0,0 +1,6 @@
+<footer>
+ <p>(c)2016 - Babbage Computing Systems</p>
+</footer>
+</body>
+
+
