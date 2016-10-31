1ad75dbd86f95cc8e737c4818847eaab1b624f9e
scottymcmahon <scottmcmahondev@outlook.com>
2016-10-31 15:44:38 +0000

    1000000th commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $rules = array(
     //
     //main pages
     //
-    'about' => "/about",
+    'about' => "/scripts/about",
     'contactus' => "/contactus",
     'blog' => "/blog",
     'blog_article' => "/blog/(?'blogID'[\w\-]+)",
